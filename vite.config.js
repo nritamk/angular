81269121f4aad9b58bad03c62e638612d7177aae
Nritam Kumar <nritamk@gmail.com>
2026-04-20 13:52:25 +0530

    vite

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  server: {
-    allowedHosts: ["devserver-main--justvish.netlify.app"],
-  },
-});
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 4200,
     open: true,
+    allowedHosts: ["devserver-main--justvish.netlify.app"],
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     emptyOutDir: true,
   },
 });
